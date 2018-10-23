7527c17afca946fe439ae37fd17cbf63ce667cc0
daniieloo <dhiaeddine.boussandel@esprit.tn>
2018-10-23 16:42:46 +0100

    2 eme commit

---
+++ Epione/.metadata/.plugins/org.eclipse.core.resources/.history/57/9017fcc6d8d600181394d54f6cc5d31e
@@ -0,0 +1,18 @@
+package en.esprit.epione.interfaces;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import tn.esprit.epione.persistance.Appointment;
+
+@Remote
+public interface AppointmentIServices {
+	
+	public void addAppointment(Appointment a );
+	public void updateAppointment(Appointment a , int id);
+	public void removeAppById(int id);
+	public List<Appointment> getAll();
+	
+	
+}
--- Epione/Epione-ejb/src/main/java/en/esprit/epione/interfaces/AppointmentIServices.java
+++ Epione/Epione-ejb/src/main/java/en/esprit/epione/interfaces/AppointmentIServices.java
@@ -1,5 +1,6 @@
 package en.esprit.epione.interfaces;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Remote;
@@ -9,10 +10,19 @@ import tn.esprit.epione.persistance.Appointment;
 @Remote
 public interface AppointmentIServices {
 	
-	public void addAppointment(Appointment a );
-	public void updateAppointment(Appointment a , int id);
-	public void removeAppById(int id);
-	public List<Appointment> getAll();
+	//Crud User
+		public void addAppointment(Appointment a );
+		public void updateAppointment(Appointment a);
+		public void removeAppById(int id);
+		public List<Appointment> getAll();
+		//Patients Services
+		public List<Appointment> MyAppointments(int idPatient);
+		public List<Appointment> MyRequests(int idPatient);
+		public void cancelRequest(int idAppointment);
+		// doctor Services
+		public List<Appointment> acceptedRequests(int idPatient);
+		//
+		public Appointment SelectAppByDay(Date date);
 	
 	
 }
