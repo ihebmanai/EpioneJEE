699c0575486270ade0b48db7364db5a43aaed936
skander1995 <b.skander@yahoo.fr>
2018-10-23 14:14:48 +0100

    first commit adding ExtractionRessources

---
+++ Epione/Epione-ejb/src/main/java/tn/esprit/epione/resources/PatientServicesImpl.java
@@ -0,0 +1,52 @@
+package tn.esprit.epione.resources;
+
+import java.util.List;
+
+import javax.ejb.Stateful;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import en.esprit.epione.interfaces.PatientServices;
+import tn.esprit.epione.persistance.Patient;
+
+
+@Stateless
+public class PatientServicesImpl implements PatientServices {
+
+	@PersistenceContext(name="Epione-ejb")
+	EntityManager em ;
+	
+	@Override
+	public void addPatient(Patient p) {
+		em.persist(p);
+		System.out.println("patient ajouté");
+		
+	}
+
+	@Override
+	public void updatePatient(Patient p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removePatient(int id) {
+		Patient p = em.find(Patient.class, id);
+		em.persist(p);
+		System.out.println("patient suprimé");
+		
+	}
+
+	@Override
+	public List<Patient> getAll() {
+		TypedQuery<Patient> query = em.createQuery("select u from Patient u ",Patient.class);	
+		 List<Patient> patients = query.getResultList();
+		 System.out.println(patients);
+		 return patients;
+		
+	}
+
+	
+}
---
+++ Epione/Epione-ejb/src/main/java/tn/esprit/epione/resources/UserServicesImpl.java
@@ -0,0 +1,57 @@
+package tn.esprit.epione.resources;
+
+import java.io.Console;
+import java.util.List;
+
+import javax.ejb.Singleton;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import en.esprit.epione.interfaces.UserServices;
+import tn.esprit.epione.persistance.User;
+
+@Singleton
+public class UserServicesImpl implements UserServices{
+
+	@PersistenceContext(unitName="Epione-ejb")
+	EntityManager em ; 
+	@Override
+	public void addUser (User u) {
+		em.persist(u);
+		System.out.println("User ajouté:");
+		
+	}
+
+	@Override
+	public void updateUser(User u,int id) {
+		User us = em.find(User.class, id);
+		us.setFirstName(u.getFirstName());
+		System.out.println("user modifié avec succées");
+		
+	}
+
+	@Override
+	public User getUserById(int id) {
+		User u = em.find(User.class, id);
+		System.out.println("user trouvé:"+u);
+		return u;
+	}
+
+	@Override
+	public void removeUser(int id) {
+		User u = em.find(User.class,id);
+		em.remove(u);
+		System.out.println("succes de suppression");
+		
+	}
+
+	@Override
+	public List<User> getAll() {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
